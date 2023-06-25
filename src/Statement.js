9c6497d937596dfc9bde4896450938b52f96f3bd
kenichi beveridge <kenbev84@gmail.com>
2023-06-25 10:23:42 +0100

    Added chalk library to print out credit transactions in green and debit in red

--- src/Statement.js
+++ src/Statement.js
@@ -1,10 +1,12 @@
+import chalk from 'chalk';
+
 class Statement {
 
     static printStatement(account) {
         console.log("\ndate       || credit  || debit  || balance")
         for (const transaction of account.getTransactions()) {
             const trans = transaction.getFullTransaction()
-            console.log(`${trans.date} || ${trans.transactionType === 'credit' ? trans.amount : '       '} || ${trans.transactionType === 'debit' ? trans.amount : '      '} || ${trans.balance}`);
+            console.log(`${trans.date} || ${chalk.green(trans.transactionType === 'credit' ? trans.amount : '       ')} || ${chalk.red(trans.transactionType === 'debit' ? trans.amount : '      ')} || ${trans.balance}`);
         }
 
     }
