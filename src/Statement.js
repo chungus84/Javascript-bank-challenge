2f9f371565202484b55399225a073d4509ea6b18
kenichi beveridge <kenbev84@gmail.com>
2023-06-26 20:12:22 +0100

    Refactored statementFormatter method

--- src/Statement.js
+++ src/Statement.js
@@ -2,25 +2,19 @@ import chalk from 'chalk';
 
 class Statement {
 
-
-
     static printStatement(account) {
         let statementArray = ["date       || credit  || debit  || balance"];
         for (const transaction of account.getTransactions()) {
             statementArray = [...statementArray, Statement.statementFormatter(transaction.getFullTransaction())]
-            //     console.log(`${trans.date} || ${chalk.green(trans.transactionType === 'credit' ? trans.amount : '       ')} || ${chalk.red(trans.transactionType === 'debit' ? trans.amount : '      ')} || ${trans.balance}`);
         }
         statementArray.forEach(transaction => console.log(transaction));
-
     }
 
     static statementFormatter(detail) {
         const emptyString = '       ';
-        return `${detail.date} || ${detail.transactionType === 'credit' ? chalk.green(detail.amount) + " ||" + emptyString : emptyString + " || " + chalk.red(detail.amount)} || ${detail.balance}`
-        // statementArray = [...statementArray, statementEntry]
-        // return statementArray
-
-
+        return `${detail.date} || ${detail.transactionType === 'credit' ?
+            chalk.green(detail.amount) + " ||" + emptyString :
+            emptyString + " || " + chalk.red(detail.amount)} || ${detail.balance}`
     }
 }
 
